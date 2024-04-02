020afd8a63397d8add0f4268aedad5892425dee3
Fabricio Souza <fabriciosouza21777@gmail.com>
2024-04-02 20:12:19 -0300

    #funcoes basicas

---
+++ goLang/fundamentos/funcoes/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	// para executar o código, execute o comando: go run *.go no diretorio do arquivo
+	// ha dois arquivos, main.go e funcoes.go
+	// tentar executar o arquivo funcoes.go não irá funcionar
+	// tentar executar o arquivo main.go não irá funcionar
+	resultado := somar(3, 4)
+	imprimir(resultado)
+}
