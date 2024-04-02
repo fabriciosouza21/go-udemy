c8414534ad7a74b2c37ad60a76087ece19ed7e18
Fabricio Souza <fabriciosouza21777@gmail.com>
2024-04-02 20:02:39 -0300

    #conversões

---
+++ goLang/fundamentos/conversoes/conversoes.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	// Go não permite conversões implicitas
+
+	x := 2.4
+	y := 2
+
+	fmt.Println(x / float64(y))
+
+	nota := 6.9
+	notaFinal := int(nota)
+	// profesor otario
+	// converte o valor para baixo
+	// 6
+	fmt.Println(notaFinal)
+
+	// cuidado...
+	// pegar o valor da tabela ASCII
+	fmt.Println("Teste " + string(97))
+
+	// int para string
+
+	fmt.Println("Teste " + strconv.Itoa(123))
+
+	// string para int
+	// retorna dois valores, primeiro o valor convertido, segundo um erro
+	num, _ := strconv.Atoi("123")
+
+	fmt.Println(num - 122)
+
+	// string para boolean
+	// 1 é verdadeiro, qual quer outro valor e falso
+	b, _ := strconv.ParseBool("true")
+
+	if b {
+		fmt.Println("Verdadeiro")
+	}
+}
