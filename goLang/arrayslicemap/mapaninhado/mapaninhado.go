e948271fdf604b060a5f0f91853e204a989b1464
Fabricio Souza <fabriciosouza21777@gmail.com>
2024-04-10 19:45:58 -0300

    #map aninhado

---
+++ goLang/arrayslicemap/mapaninhado/mapaninhado.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	funcsPorLertra := map[string]map[string]float64{
+		"G": {
+			"Gabriel Silva": 15456.78,
+			"Guga Pereira":  8456.78,
+		},
+		"J": {
+			"José João": 11325.45,
+		},
+		"P": {
+			"Pedro Junior": 1200.00,
+		},
+	}
+
+	delete(funcsPorLertra, "P")
+
+	for let, funcs := range funcsPorLertra {
+		fmt.Println(let, funcs)
+	}
+}
