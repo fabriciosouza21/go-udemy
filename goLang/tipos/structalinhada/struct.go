61e2eeb2418fb83f1f8914e7caf54211bd1139e7
Fabricio Souza <fabriciosouza21777@gmail.com>
2024-04-27 10:02:27 -0300

    #struct alinhada

---
+++ goLang/tipos/structalinhada/struct.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type item struct {
+	produtoID int
+	qtde      int
+	preco     float64
+}
+
+type pedido struct {
+	userId int
+	itens  []item
+}
+
+// receiver (p pedido)
+func (p pedido) valorTotal() float64 {
+	total := 0.0
+	for _, item := range p.itens {
+		total += item.preco * float64(item.qtde)
+	}
+	return total
+}
+
+func main() {
+
+	// priorize a forma literal
+	// torna o código mais legível
+	pedido := pedido{
+		userId: 1,
+		itens: []item{
+			{1, 2, 12.10},
+			{2, 1, 23.49},
+			{11, 100, 3.13},
+		},
+	}
+
+	fmt.Printf("Valor total do pedido é R$ %.2f", pedido.valorTotal())
+
+}
