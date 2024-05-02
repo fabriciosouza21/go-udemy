6224e0fb34a0510acbfaa76ee1baf49f98dd03c2
Fabricio Souza <fabriciosouza21777@gmail.com>
2024-05-02 20:08:12 -0300

    #bloqueante

--- goLang/concorrencia/bloqueio/bloqueio.go
+++ goLang/concorrencia/bloqueio/bloqueio.go
@@ -12,8 +12,11 @@ func rotina(c chan int) {
 }
 
 func main() {
+	// quando criar um canal sem buffer, a operação de escrita no canal é bloqueante
 	c := make(chan int) // canal sem buffer
 	go rotina(c)
+	// quadno não tem buffer, a operação de leitura é bloqueante
+	//
 	fmt.Println(<-c) // operação bloqueante
 	fmt.Println("Foi lido")
 	fmt.Println(<-c) // deadlock
