6e53af9108dfaa218056869ec8276b5ea7ad6ecd
Fabricio Souza <fabriciosouza21777@gmail.com>
2024-05-18 16:20:38 -0300

    #pacotes

--- goLang/concorrencia/primos/primos.go
+++ goLang/concorrencia/primos/primos.go
@@ -2,39 +2,13 @@ package main
 
 import (
 	"fmt"
-	"time"
+
+	pm "curso.com/m/primos"
 )
-
-func isPrimo(num int) bool {
-	for i := 2; i < num; i++ {
-		if num%i == 0 {
-			return false
-		}
-	}
-	return true
-}
-
-// Primos - função que retorna um canal de inteiros
-func primos(n int, c chan int) {
-	inicio := 2
-	for i := 0; i < n; i++ {
-		for primo := inicio; ; primo++ {
-			if isPrimo(primo) {
-				c <- primo
-				inicio = primo + 1
-				time.Sleep(time.Millisecond * 100)
-				break
-			}
-		}
-	}
-	// fecha o canal
-	// se não fecha pode gerar um loop infinito
-	close(c)
-}
 
 func main() {
 	c := make(chan int, 30)
-	go primos(cap(c), c)
+	go pm.Primos(cap(c), c)
 	for i := range c {
 		fmt.Printf("%d ", i)
 	}
---
+++ goLang/primos/primos.go
@@ -0,0 +1,30 @@
+package primos
+
+import "time"
+
+func isPrimo(num int) bool {
+	for i := 2; i < num; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// Primos - função que retorna um canal de inteiros
+func Primos(n int, c chan int) {
+	inicio := 2
+	for i := 0; i < n; i++ {
+		for primo := inicio; ; primo++ {
+			if isPrimo(primo) {
+				c <- primo
+				inicio = primo + 1
+				time.Sleep(time.Millisecond * 100)
+				break
+			}
+		}
+	}
+	// fecha o canal
+	// se não fecha pode gerar um loop infinito
+	close(c)
+}
